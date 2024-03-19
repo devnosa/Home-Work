8dbb7e7ecc0f419af351439b59ac1c3532af703e
devnosa <kdevnosadze@yahoo.com>
2024-03-19 12:08:22 +0100

    RestController

---
+++ src/main/java/com/example/Hello/Tom/HelloTomApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hello.Tom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hello/Tom/HelloTomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hello.Tom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
